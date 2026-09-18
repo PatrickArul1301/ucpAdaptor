5d5f6fd9112e207e8ff59fa4c17e85a7479bc76e
Patrick Arul <patrick.arul.prasad@accenture.com>
2026-09-18 12:11:23 +0800

    UCP Json Key update - 10

--- src/main/java/com/accenture/UCPAdaptor/CatalogSearchTool.java
+++ src/main/java/com/accenture/UCPAdaptor/CatalogSearchTool.java
@@ -2,6 +2,7 @@ package com.accenture.UCPAdaptor;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
